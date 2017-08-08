6fe220dfaed90b8b18ec469b752646f30cb0372c
gravenguan <graven@tamu.edu>
2017-08-08 17:06:44 -0500

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello, Ken');
+  res.send('hello, Plano');
 });
 
 app.listen(process.env.PORT || 3000);
