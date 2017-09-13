5fd99f01f0c94a860fa3d4b277989390d0ab5d57
gravenguan <graven@tamu.edu>
2017-09-13 09:37:15 -0500

    Update Pipline

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Testing Heroku Pipline');
+  res.send('OCIO Pipline testing');
 });
 
 app.listen(process.env.PORT || 3000);
