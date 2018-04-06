68720f98c4252a2c5f2f06d70b69b8d7ac8909b3
gravenguan <graven@tamu.edu>
2018-04-06 15:59:11 -0500

    update test

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('OCIO Pipline testing_Heran_3_29');
+  res.send('Test_4_6_2018');
 });
 
 app.listen(process.env.PORT || 3000);
