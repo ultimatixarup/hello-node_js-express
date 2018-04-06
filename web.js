d4cf2668bc265f2ca2efc9ce746c80c0f1412a9f
gravenguan <graven@tamu.edu>
2018-04-06 16:20:32 -0500

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Testing Heroku Pipline');
+  res.send('Testing Heroku Pipline 4_6_2018');
 });
 
 app.listen(process.env.PORT || 3000);
